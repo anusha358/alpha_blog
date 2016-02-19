f943178af0004074ac90efdeccb31608fcb234f0
anusha <anushapradeep358@gmail.com>
2016-02-19 08:11:17 +0000

    added user screen

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,24 @@ class UsersController < ApplicationController
     else
       render 'new'
     end
+  end
+  def show
+      @user = User.find(params[:id])
+  end
+  def edit
+    @user= User.find( params[:id])
     
+  end
+  def update
+    
+    @user= User.find( params[:id])
+    if @user.update_attributes(:password => params[:password])
+      flash[:success] = "password sucessfully updated"
+       redirect_to user_path(@user)
+    else
+      render 'edit'
+      
+    end
   end
   def user_params
     params.require(:user).permit(:username,:email,:password)
