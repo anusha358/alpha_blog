727437fd12b0e3d1ccd8266ac727c03e41fa9cf6
anusha <anushapradeep358@gmail.com>
2016-03-09 01:37:37 +0000

    completed integration test

---
+++ test/controllers/categories_controller_test.rb
@@ -0,0 +1,22 @@
+require 'test_helper'
+class CategoriesControllerTest < ActionController::TestCase
+def setup
+@category = Category.create(name: "sports")
+@user = User.create(username: "anushapradeep", email: "anushachokka@gmail.com", password: "Prad@2015", admin: true)
+end
+test "should get categories index" do
+get :index
+assert_response :success
+end
+test "should get new" do
+session[:user_id] = @user.id
+get :new
+assert_response :success
+end
+test "should get show" do
+get(:show, {'id' => @category.id})
+assert_response :success
+end
+
+
+end
