1a423c900cbcf916f011142bed8162dae22ef040
anusha <anushapradeep358@gmail.com>
2016-03-10 01:26:36 +0000

    updated validations of articles

--- app/models/article.rb
+++ app/models/article.rb
@@ -2,7 +2,7 @@ class Article < ActiveRecord::Base
   belongs_to :user
   has_many :article_categories
   has_many :categories, through: :article_categories
-  validates:title, presence:true , length:{minimum:3 , maximum:15}
-  validates:description, presence:true , length:{minimum:3 , maximum:25}
+  validates:title, presence:true , length:{minimum:3 , maximum:30}
+  validates:description, presence:true , length:{minimum:5 , maximum:50}
   validates:user_id, presence:true
 end
